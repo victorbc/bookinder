53638b802ef0d66eb3b1b7ce518f65c99245ec29
Nicolas <nicolasgabrielt@gmail.com>
2016-03-17 02:18:13 -0300

    Tela de seleção de categorias

---
+++ app/src/main/java/equipe/projetoes/Activities/CategoriasActivity.java
@@ -0,0 +1,69 @@
+package equipe.projetoes.Activities;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.siyamed.shapeimageview.CircularImageView;
+import com.github.siyamed.shapeimageview.ShapeImageView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import equipe.projetoes.Adapters.CategoriasGridViewAdapter;
+import equipe.projetoes.Models.Categoria;
+import equipe.projetoes.R;
+
+public class CategoriasActivity extends Activity{
+    private static List<Categoria> mCategorias;
+    private List<Categoria> isCategoriaSelecionada = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categorias);
+        mCategorias = Arrays.asList(Categoria.values());
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+        GridView gridview = (GridView) findViewById(R.id.itens_categorias);
+        gridview.setAdapter(new CategoriasGridViewAdapter(this, mCategorias));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(CategoriasActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+
+                TextView nome = (TextView) v.findViewById(R.id.nome_categoria);
+                CircularImageView img = (CircularImageView) v.findViewById(R.id.imagem_categoria);
+
+                if(!isCategoriaSelecionada.contains(mCategorias.get(position))) {
+                    nome.setTextAppearance(getApplicationContext(),R.style.boldTextVerde);
+                    img.setBorderColor(getResources().getColor(R.color.green));
+                    isCategoriaSelecionada.add(mCategorias.get(position));
+                }else{
+                    nome.setTextAppearance(getApplicationContext(), R.style.normalText);
+                    img.setBorderColor(getResources().getColor(R.color.branco));
+                    isCategoriaSelecionada.remove(mCategorias.get(position));
+                }
+
+            }
+        });
+
+    }
+
+
+
+}
---
+++ app/src/main/java/equipe/projetoes/Models/Categoria.java
@@ -0,0 +1,71 @@
+package equipe.projetoes.Models;
+
+import android.app.Activity;
+import android.content.res.Resources;
+
+import java.security.AccessControlContext;
+import java.text.Normalizer;
+
+/**
+ * Created by Nicolas on 16/03/2016.
+ */
+
+/**
+ * Enum que guarda caracteristicas das categorias dos livros
+ */
+public enum Categoria {
+
+    ROMANCE("Romance"), fANTASIA("Fantasia"),MISTERIO("Mistério"),FICCAOCIENTIFICA("Ficção \n Científica","scifi"),
+    INFANTIL("Infantil"),MANGA("Mangá"),DIDATICO("Didático"),QUADRINHOS("Quadrinhos"),AUTOAJUDA("Auto-Ajuda","autoajuda"),
+    RELIGIOSO("religioso");
+
+    private final String nome;
+    private final String nomeImagem;
+    private final static String IMAGEM_CATEGORIA = "categoria_img_";
+    private final static String DRAWABLE = "drawable";
+
+    Categoria(String nome){
+        this.nome = nome;
+        this.nomeImagem = nome;
+    }
+
+    Categoria(String nome , String nomeImagem){
+        this.nome = nome;
+        this.nomeImagem = nomeImagem;
+    }
+
+
+    private String getNomeImagem(){
+        return nomeImagem;
+    }
+
+
+    /**
+     *
+     * @return String
+     */
+    public String getNome(){
+        return nome;
+    }
+
+    /**
+     * Fornece o id do tipo int da imagem associada a categoria
+     * @param activity
+     * @return id da imagem da categoria que é do tipo int
+     */
+    public int getImagem(Activity activity){
+        Resources mResources = activity.getResources();
+        String mPackageName = activity.getPackageName();
+
+
+
+        final int categoryImageResource = mResources.getIdentifier(
+                IMAGEM_CATEGORIA + (removerAcentos(getNomeImagem()).toLowerCase()), DRAWABLE, mPackageName);
+        return  categoryImageResource;
+    }
+
+    public static String removerAcentos(String str) {
+        return Normalizer.normalize(str, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+    }
+
+}
