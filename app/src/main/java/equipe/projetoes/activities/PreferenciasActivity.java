f99625f3f63cbd8f38fc06d7248173b8be029d06
Luiz Henrique <Luiz Henrique>
2016-09-06 13:05:08 -0300

    Tela preferencias

---
+++ app/src/main/java/equipe/projetoes/activities/PreferenciasActivity.java
@@ -0,0 +1,13 @@
+package equipe.projetoes.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreferenciasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferencias);
+    }
+}
