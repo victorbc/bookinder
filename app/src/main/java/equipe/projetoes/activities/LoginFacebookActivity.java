cb0addd424f3ffe25f8b820bade730386194258a
Luiz Henrique <Luiz Henrique>
2016-09-11 16:19:35 -0300

    Associar conta com facebook

---
+++ app/src/main/java/equipe/projetoes/activities/LoginFacebookActivity.java
@@ -0,0 +1,12 @@
+package equipe.projetoes.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginFacebookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
